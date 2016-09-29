2947a4f94e7a5175e8e7db2d63863b9d6ff6f29f
Ben Allen <ben.allen2@gmail.com>
2016-09-29 15:11:55 -0400

    functional but ugly

--- twitch2.js
+++ twitch2.js
@@ -1,3 +1,10 @@
+var gameArr = [];
+var game = "League of Legends";
+var listClick = "";
+var streamersArr = [];
+var streamClick = "";
+
+
 function menuDrop () {
   $(".topGames").click(function(){
     $(".fa-arrow-circle-down").toggleClass("iconRotate");
@@ -15,9 +22,56 @@ function getGames() {
     },
     success: function (data) {
       for (var i = 0; i < data.top.length; i++){
-      $(".gamesMenu").append("<li class='listGame'><img src='" + data.top[i].game.box.medium + "'></br>" + data.top[i].game.name + "</br>Viewers: " + data.top[i].viewers);
+      $(".gamesMenu").append("<li class='listGame' id='"+ i + "'><img src='" + data.top[i].game.box.medium + "'></br>" + data.top[i].game.name + "</br>Viewers: " + data.top[i].viewers);
+      gameArr.push(data.top[i].game.name);
+
       }
-      console.log(data);
+      gameSelect();
+      console.log(gameArr);
+    }
+  });
+}
+
+function gameSelect () {
+  $(".listGame").click(function() {
+    listClick = $(this).attr("id");
+    game = gameArr[Number(listClick)];
+    getStreams();
+  });
+}
+
+function streamSelect () {
+  $(".streamer").click(function() {
+    streamClick = $(this).attr("streamer");
+    streamer = streamersArr[Number(streamClick)];
+    console.log(streamer);
+    $("#streamVideo").empty();
+    player();
+    $("#currentName").html(streamer);
+  });
+}
+
+var streamer = "Twitch";
+
+function getStreams(){
+  $.ajax({
+    method: "GET",
+    url: "https://api.twitch.tv/kraken/streams?game=" + game +"&limit=10",
+    headers: {
+      "Client-ID": "g0yy7wimbymfcnmott7o2dgatvwnbwn"
+    },
+    success: function (data) {
+      for (var k = 0; k < data.streams.length; k++){
+        streamersArr.push(data.streams[k].channel.name);
+      }
+      console.log(streamersArr);
+      for(var i = 0; i < 5; i++){
+        $(".streamersList1").append("<li class='streamer' streamer='" + i + "'><img src='" + data.streams[i].preview.small + "'></br>" + data.streams[i].channel.display_name + "</br>Viewers: " + data.streams[i].viewers);
+      }
+      for(var j = 5; j < 10; j++){
+        $(".streamersList2").append("<li class='streamer' streamer='" + j + "'><img src='" + data.streams[j].preview.small + "'></br>" + data.streams[j].channel.display_name + "</br>Viewers: " + data.streams[j].viewers);
+      }
+      streamSelect();
     }
   });
 }
@@ -26,14 +80,16 @@ function player() {
   var options = {
         width: 854,
         height: 480,
-        channel: "{CHANNEL}",
+        channel: "",
         //video: "{VIDEO_ID}"
     };
-    var player = new Twitch.Player("{PLAYER_DIV_ID}", options);
-    player.setVolume(0.5);
+    var player = new Twitch.Player("streamVideo", options);
+    player.setVolume(0);
+    player.setChannel(streamer);
 }
 
 $(document).ready(function() {
   menuDrop();
   getGames();
+  player();
 });
